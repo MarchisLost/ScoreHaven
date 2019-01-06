e2ac5f2018bb082c2471c702f876c5383e70661d
MarchisLost <MarchisLost@users.noreply.github.com>
2019-01-06 16:34:43 +0000

    nada demais

--- ScoreHaven/reset_pwd.php
+++ ScoreHaven/reset_pwd.php
@@ -5,6 +5,49 @@
         $pwd1 = $_POST["pwd"];
         $pwd2 = $_POST["pwd2"];
 
+        if(empty($pwd) || empty($pwd2)){
+            header("Location: ../sign_in.php");
+            exit();
+
+        }elseif($pwd != $pwd2){
+            header("Location: ../sign_in.php");
+            exit();
+        }
+
+        $currentDate= date("U");
+        require "conecta_bd.php";
+
+        $sql= "SELECT * FROM pwdReset WHERE pwdResetSelector=? AND pwdResetExpires >=?";
+
+        $stmt = mysqli_stmt_init($ligacao);
+
+        if(!mysli_stmt_prepare($stmt, $sql)){
+            echo "Error";
+            exit();
+        } else{
+            mysqli_stmt_bin_param($stmt, "s", $selector);
+            mysqli_stmt_execute($stmt);
+
+            $result = mysqli_stmt_get_result($stmt);
+            if(!$row = mysqli_fetch_assoc($result)){
+                echo "You need to re-submit your reset request.";
+                exit();
+            }else{
+                $tokenBin = hex2bin($validator);
+                $tokenCheck = password_verify($tokenBin, $row["pwdResetToken"]);
+
+                if($tokenCheck === false){
+                    echo "You need to re-submit your reset request.";
+
+                }elseif($tokenCheck === true){
+                    $tokenEmail = $row["pwdResetEmail"];
+
+                    $sql = "SELECT * FROM users WHERE emailUsers=?;";
+
+                }
+            }
+        }
+
     }else{
         header("Location: ../index.php");
     }
